98ff696e2edffb0f630d0760ab99b008d8bd1f20
Travis Lang <tlang50@icloud.com>
2019-01-21 19:38:29 -0600

    build app

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d4d03ca7aac320c6ce0ed2063bff4c7.js"
+  "/precache-manifest.22abf3907d2f1f2ed11ff85f86ee6103.js"
 );
 
 workbox.clientsClaim();
