cd22c418e5fbd409443db592d4a439c5fe005bf7
Travis Lang <tlang50@icloud.com>
2019-02-13 16:10:56 -0600

    update database.sql file

--- database.sql
+++ database.sql
@@ -4,23 +4,34 @@ CREATE TABLE "person" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (80) NOT NULL,
     "username" VARCHAR (80) UNIQUE,
+    "alerts_total" INT DEFAULT 0,
+    "socket" VARCHAR(100),
     "password" VARCHAR (1000),
     "facebook_id" VARCHAR (80) UNIQUE,
+    "facebook_image" VARCHAR(150) NOT NULL DEFAULT 'images/facebook-avatar.jpg',
     "fb_access_token" VARCHAR (250) UNIQUE,
-    "facebook_image" VARCHAR(150) NOT NULL DEFAULT 'images/facebook-avatar.jpg';
+    "push_endpoint" VARCHAR(200),
+    "p256dh" VARCHAR(200),
+    "auth" VARCHAR(200)
 );
+
 
 CREATE TABLE "portfolio" (
 	"id" SERIAL PRIMARY KEY,
-	"portfolio_name" VARCHAR (80) NOT NULL,
-	"person_id" INT REFERENCES "person"
+	"portfolio_name" VARCHAR (80) NOT NULL DEFAULT 'My First Portfolio',
+	"person_id" INT REFERENCES "person" NOT NULL
 );
 
 CREATE TABLE "symbols" (
 	"id" SERIAL PRIMARY KEY,
-	"symbol"  VARCHAR (20) UNIQUE NOT NULL,
+	"symbol"  VARCHAR (20),
 	"logo" VARCHAR(250),
-	"symbol_name" VARCHAR (80) NOT NULL
+	"symbol_name" VARCHAR (80),
+	"base_asset" VARCHAR(25),
+	"quote_asset" VARCHAR(25),
+	"last_price" VARCHAR(50),
+	"volume" VARCHAR(50),
+	"price_change" VARCHAR(25)
 );
 
 CREATE TABLE "portfolio_symbols" (
@@ -34,5 +45,8 @@ CREATE TABLE "alerts" (
 	"person_id" INT NOT NULL REFERENCES "person",
 	"price_threshold" DECIMAL NOT NULL,
 	"less_than" BOOLEAN NOT NULL,
-	"symbol_id" INT REFERENCES "symbols"
+	"symbol_id" INT REFERENCES "symbols",
+	"alerts_on" BOOLEAN NOT NULL DEFAULT TRUE,
+	"alert_sent" VARCHAR(150)
+	"order" INT
 );
